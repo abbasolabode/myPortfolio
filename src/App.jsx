c6ee6251cadeee9a99d0a7c362495936652a7aa9
Abbas Olabode <olabodeabbas80@outlook.com>
2026-05-20 12:38:07 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
+        {import.meta.env.DEV && (<ReactQueryDevtools initialIsOpen={false} />)}
         <BrowserRouter>
           <Routes>
             <Route element={<AppLayouts />}>
