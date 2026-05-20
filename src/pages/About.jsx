f1c4df4a7518936d9ee7829604472c3f4937f76d
Abbas Olabode <olabodeabbas80@outlook.com>
2026-05-20 14:30:27 +0100

    fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import AboutUi from "../ui/AboutUi";
 
 export default function About() {
   return (
-    <div className=" bg-linear-to-br from-black via-zinc-950 to-black">
+    <div className="px-6 bg-linear-to-br from-black via-zinc-950 to-black">
       <AboutUi />
     </div>
 
