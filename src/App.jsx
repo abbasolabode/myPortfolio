150edfcc0b1e44f0e0b7413dd3a73e3727f39eb2
abbasolabode <olabodeabbas80@outlook.com>
2026-06-20 20:30:13 -0700

    Restore ReactQueryDevtools for development only

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppLayouts from "./layouts/AppLayouts";
@@ -25,6 +26,7 @@ export default function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
+        {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
         <BrowserRouter>
           <Routes>
             <Route element={<AppLayouts />}>
