0e3522a1cbb0ae82853ea6a753955b07f07d2853
abbasolabode <olabodeabbas80@outlook.com>
2026-06-20 20:26:42 -0700

    Remove ReactQueryDevtools and add favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Toaster } from "react-hot-toast";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppLayouts from "./layouts/AppLayouts";
@@ -26,9 +25,6 @@ export default function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        {import.meta.env.DEV && (
-          <ReactQueryDevtools initialIsOpen={false} />
-        )}
         <BrowserRouter>
           <Routes>
             <Route element={<AppLayouts />}>
